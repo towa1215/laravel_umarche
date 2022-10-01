bdeb64efe11a3599cc2c415bc33379c9b2de5e7e
Towa Kikuchi <107536316+towa1215@users.noreply.github.com>
2022-10-01 13:56:01 +0900

    フォームリクエスト

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -8,7 +8,7 @@ use App\Models\Shop;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use InterventionImage;
-use App\Http\Requests\UploodImageRequest;
+use App\Http\Requests\UploadImageRequest;
 
 class ShopController extends Controller
 {
@@ -50,7 +50,7 @@ class ShopController extends Controller
         return view('owner.shops.edit', compact('shop'));
     }
 
-    public function update(UploodImageRequest $request, $id)
+    public function update(UploadImageRequest $request, $id)
     {
         $imageFile = $request->image;
         if(!is_null($imageFile) && $imageFile->isValid() ){
@@ -58,7 +58,7 @@ class ShopController extends Controller
             $fileName = uniqid(rand().'_');
             $extension = $imageFile->extension();
             $fileNameToStore = $fileName.'.'.$extension;
-            $resizedImage = \InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+            $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
             dd($imageFile, $resizedImage);
 
             Storage::put('public/shops'. $fileNameToStore, $resizedImage);
