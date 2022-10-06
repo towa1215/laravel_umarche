b2c16380847783cf486b9922269fb842f1c77378
Towa Kikuchi <107536316+towa1215@users.noreply.github.com>
2022-10-06 14:30:54 +0900

    stock

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\Shop;
 use App\Models\SecondaryCategory;
 use App\Models\Image;
+use App\Models\Stock;
 
 class Product extends Model
 {
@@ -25,6 +26,11 @@ class Product extends Model
     public function shop()
     {
         return $this->belongsTo(Shop::class);
+    }
+
+    public function stock()
+    {
+        return $this->hasMany(Stock::class);
     }
     
 }
